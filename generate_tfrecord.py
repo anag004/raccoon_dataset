000e4cd7e7ef2936295eb1cb658ba37686782eed
anag004 <34573805+anag004@users.noreply.github.com>
2018-12-22 03:12:08 +0530

    Added the image_dir flag in the usage example
    In most object detection workflows, image data is usually stored in an images directory with separate train and test folders. Explicitly wrote this using the --image_dir flag to indicate to the user that this is what the script assumes. This also indicates to the user that if the images are in a different directory, then it must be specified using this flag.
    
    Also corrected the output path so that script writes in a data directory (as is the convention).

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -2,10 +2,10 @@
 Usage:
   # From tensorflow/models/
   # Create train data:
-  python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=train.record
+  python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=data/train.record --image_dir=images/train/
 
   # Create test data:
-  python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=test.record
+  python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=data/test.record --image_dir=images/test/
 """
 from __future__ import division
 from __future__ import print_function
